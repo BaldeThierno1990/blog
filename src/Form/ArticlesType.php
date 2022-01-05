14a34198e7281b4af79103330188ecfb62a60abd
BaldeThierno1990 <ibrahimabotore@gmail.com>
2022-01-05 11:58:45 +0100

    first commit

---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('slug')
+            ->add('contenu')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('imageFile', FileType::class,[
+                'required'=> false
+            ])
+            ->add('users')
+            ->add('mots_cles')
+            ->add('categories')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
